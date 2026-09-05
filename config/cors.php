96299fa1a9826bbf1f2eeee021148a75cc40b6ed
Jenothan <esanjenothan@gmail.com>
2026-09-06 01:49:46 +0530

    allowed_origins updated

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://keysportsclub.netlify.app'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://keysportsclub.netlify.app', '*'],
 
     'allowed_origins_patterns' => ['.*'],
 
